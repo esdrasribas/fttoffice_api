6cd23c05c13141bd6b0b73afe8f583063d6c5736
Esdras Ribas <55923654+esdrasribas@users.noreply.github.com>
2024-11-12 23:29:01 -0300

    Update VQIP_RetrieveIPv4address.py

--- api/fttoffice/libs/VQIP_RetrieveIPv4address.py
+++ api/fttoffice/libs/VQIP_RetrieveIPv4address.py
@@ -5,7 +5,7 @@ from fttoffice.libs.exceptError import WS_OBJECT_NAME_NOT_FOUND
 class RetrieveIPv4address:
     def Retrieve_IPv4_address(self, GPONID):
         try:
-            url = "http://10.61.184.101:8080/ws/services/VQIPWebService/"
+            url = "URL"
             headers = {
                 'Content-Type': 'text/xml; charset=utf-8'
             }
@@ -49,8 +49,8 @@ class RetrieveIPv4address:
 
 
     def xml_Retrieve_IPv4_address(self, GPONID):
-        user = "vt38823"
-        senha = "abcd12345"
+        user = "XXXX"
+        senha = "XXXXX"
         return f"""<?xml version="1.0" encoding="UTF-8"?>
         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
             <soapenv:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
@@ -60,14 +60,14 @@ class RetrieveIPv4address:
                         <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">{senha}</wsse:Password>
                     </wsse:UsernameToken>
                 </wsse:Security>
-                <wsa:To>http://10.61.184.101:8080/ws/services/VQIPWebService</wsa:To>
+                <wsa:To>URL</wsa:To>
                 <wsa:MessageID>urn:uuid:fe369f95-9fa0-4800-9af4-f079456e1369</wsa:MessageID>
                 <wsa:Action>VQIPManager_GetRequest</wsa:Action>
             </soapenv:Header>
             <soapenv:Body>
                 <ns1:GetRequest xmlns:ns1="http://alcatel-lucent.com/qip/nb/ws">
                     <ns1:commonParam>
-                        <ns1:organization>OI_Regiao_1</ns1:organization>
+                        <ns1:organization></ns1:organization>
                         <ns1:locale>pt_BR</ns1:locale>
                     </ns1:commonParam>
                     <ns1:reqObject xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="ns1:V4_ADDR_REC">
