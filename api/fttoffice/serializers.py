d60a293b551a8be934e09213e2601080f3161a6f
Esdras Ribas <55923654+esdrasribas@users.noreply.github.com>
2024-11-12 23:34:56 -0300

    Update serializers.py

--- api/fttoffice/serializers.py
+++ api/fttoffice/serializers.py
@@ -1,13 +1,63 @@
 from rest_framework import serializers
+from django.core.validators import RegexValidator
 
-class CadastroIpv4Serializer(serializers.Serializer):
+
+class CadastrarAprovisionamentoSerializer(serializers.Serializer):
     elemento = serializers.CharField()
     gponid = serializers.CharField()
+    uf = serializers.CharField(
+            validators=[
+                RegexValidator(
+                regex='^[A-Za-z]{2}$',
+                message='O campo UF deve ter exatamente 2 letras.'
+            )
+        ]
+    )
+
+class CadastroIpv6Serializer(serializers.Serializer):
+    elemento = serializers.CharField()
+    gponid = serializers.CharField()
+    uf = serializers.CharField(
+            validators=[
+                RegexValidator(
+                regex='^[A-Za-z]{2}$',
+                message='O campo UF deve ter exatamente 2 letras.'
+            )
+        ]
+    )
 
 
-class ConsultaIpv4Serializer(serializers.Serializer):
+class ConsultaSerializer(serializers.Serializer):
+    gponid = serializers.CharField()
+    uf = serializers.CharField(
+            validators=[
+                RegexValidator(
+                regex='^[A-Za-z]{2}$',
+                message='O campo UF deve ter exatamente 2 letras.'
+            )
+        ]
+    )
+
+
+
+class DeleteSerializer(serializers.Serializer):
+    gponid = serializers.CharField()
+    uf = serializers.CharField(
+            validators=[
+                RegexValidator(
+                regex='^[A-Za-z]{2}$',
+                message='O campo UF deve ter exatamente 2 letras.'
+            )
+        ]
+    )
+
+
+
+class TaskResultSerializer(serializers.Serializer):
+    ipv4 = serializers.CharField()
     gponid = serializers.CharField()
 
 
-class DeleteIpv4Serializer(serializers.Serializer):
-    ipv4 = serializers.CharField()
+class ConsultarTarefasSerializer(serializers.Serializer):
+    task_id = serializers.UUIDField()
+
