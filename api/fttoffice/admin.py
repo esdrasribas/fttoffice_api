720b8f6b41e7103018e1862192c74f514b0db459
Esdras Ribas <55923654+esdrasribas@users.noreply.github.com>
2024-11-12 23:35:38 -0300

    Update admin.py

--- api/fttoffice/admin.py
+++ api/fttoffice/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('start_time',)
+    fields = ('elemento', 'gponid', 'uf', 'subnet','subnetMask', 'ipv4', 'gponid_wan', 'ipv6_wan', 'gponid_lan', 'ipv6_lan', 'start_time', 'end_time', 'traceback', 'status')
+    list_display = ('task_id', 'elemento', 'gponid', 'status')
+
+
+admin.site.register(Task, TaskAdmin)
+
